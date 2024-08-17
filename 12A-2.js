843f404cd0e9b5316b9d50b392f7386190fb7de9
masreyn <49424309+masreyn@users.noreply.github.com>
2024-08-17 18:51:28 -0500

    Add files via upload

---
+++ 12A-2.js
@@ -0,0 +1,121 @@
+// Assigment 12A
+// class -> https://tonejs.github.io/docs/r13/Tone
+// constructors -> https://tonejs.github.io/docs/
+// https://tonejs.github.io/ set tones to master
+// Tone.Synth is a basic synthesizer with a single envelope and single oscillator.
+/*  ADSR envelope component ▵ - triggerAttackRelease is a combination of two methods:
+   triggerAttack when the amplitude >↑ and triggerRelease <↓ amplitude 
+   Tone.js standard piano octave notes set to note frequencies */
+// keycodes https://keycode.info/
+// https://www.lynda.com/Ableton-Live-tutorials/Using-amp-envelope-shape-volume/428720/466104-4.html
+// https://p5js.org/reference/#/p5/midiToFreq
+// https://www.guitarland.com/MusicTheoryWithToneJS/EditSynth.html
+// https://newt.phys.unsw.edu.au/jw/notes.html
+
+const c = ['C4'];
+const d = ['D4'];
+const e = ['E4'];
+const f = ['F4'];
+const g = ['G4'];
+const a = ['A4'];
+const b = ['B4'];
+const h = ['C5'];
+
+// var tremolo = new Tone.Tremolo(9, 1).toMaster().start(); -- > needs constructor
+// var osc = new Tone.Oscillator(440, "sine").toMaster().start(); //  <---THIS IS A TEST!! Musical note 'A' ocatve 4 440 freq sounds correct
+var osc = new Tone.Oscillator().connect(ampEnv).start();
+// trigger the envelopes attack and release "8t" apart
+ampEnv.triggerAttackRelease("2t");
+
+function setup () {
+  createCanvas(800, 700);
+  poly = new Tone.PolySynth(6, Tone.Synth).toMaster();
+  poly.set({
+    oscillator: {
+      type: 'fatsawtooth'
+    }
+  });
+// ampEnvelope
+  ampEnvelope = new Tone.AmplitudeEnvelope({
+    "attack": 0.1,
+    "decay": 0.5, // 0.5,
+    "sustain": 1.0, //(normal range 0.0 - 1.0)
+    "release": 0.8,
+  }).toMaster();
+}
+function draw () {
+  background(189, 195, 199);
+  background(54, 69, 79);
+  fill(255);
+  textStyle(BOLD);
+  textAlign(CENTER);
+  textSize(34);2
+  text('Play an Octave - Press Number Keys 1 - 8', width / 2, 585);
+  textSize(18);
+  text('Scale Starts @ Piano Middle C ( no sharps or flats )', width / 2, 620);
+  // map the notes/keys/sound to the shapes
+  if (keyCode === 49) {
+    noStroke();
+    fill(247, 9, 219);
+    ellipse(125, 450, 90, 90);
+    textSize(32);
+    text('C', 125, 529);
+  } else if (keyCode === 50) {
+    noStroke();
+    fill('LawnGreen');
+    ellipse(220, 400, 95, 80);
+    textSize(32);
+    text('D', 220, 475);
+  } else if (keyCode === 51) {
+    fill('DeepSkyBlue');
+    rect(275, 310, 88, 88, 20);
+    textSize(32);
+    text('E', 319, 435);
+  } else if (keyCode === 52) {
+    // fill('OrangeRed');
+    fill(241, 90, 34);
+    ellipse(375, 315, 90);
+    textSize(32);
+    text('F', 375, 393);
+  } else if (keyCode === 53) {
+    fill(171, 224, 66); 
+    fill('lime');
+    rect(400, 230, 88, 88, 20);
+    textSize(32);
+    text('G', 444, 353);
+  } else if (keyCode === 54) {
+    fill(255, 165, 0);
+    ellipse(499, 230, 94, 80);
+    textSize(32);
+    text('A', 499, 304);
+  } else if (keyCode === 55) {
+    fill('Fuchsia');
+    ellipse(590, 195, 90);
+    textSize(32);
+    text('B', 593, 273);
+  } else if (keyCode === 56) {
+    fill('DarkTurquoise');
+    rect(650, 100, 88, 88, 20);
+    textSize(32);
+    text('C', 695, 223);
+  }
+} // end fnx draw
+function keyPressed () {
+  if (keyCode === 49) {
+    poly.triggerAttackRelease(c, 0.60);
+  } else if (keyCode === 50) {
+    poly.triggerAttackRelease (d, 0.60);
+  } else if (keyCode === 51) {
+    poly.triggerAttackRelease(e, 0.60);
+  } else if (keyCode === 52) {
+    poly.triggerAttackRelease(f, 0.60);
+  } else if (keyCode === 53) {
+    poly.triggerAttackRelease(g, 0.60);
+  } else if (keyCode === 54) {
+    poly.triggerAttackRelease(a, 0.60);
+  } else if (keyCode === 55) {
+    poly.triggerAttackRelease(b, 0.60);
+  } else if (keyCode === 56) {
+    poly.triggerAttackRelease(['C5', 'E4', 'G4'], 0.6);
+  }
+}
